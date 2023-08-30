70dc4fc02ef032b6095472ce5e7632449055f093
Vladimir <le0679993@crc-lennox.qc.ca>
2023-08-30 12:15:56 -0400

    Added the hands and the movement for pinching and grabbing

---
+++ Assets/AnimateHandsOnInput.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandsOnInput : MonoBehaviour
+{
+    public Animator handAnimator;
+    public InputActionProperty pinchAnimationAction;
+    public InputActionProperty gripAnimationAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+        //Debug.Log(triggerValue);
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+    }
+}
