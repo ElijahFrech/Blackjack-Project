e48b6a4209f27ccd7ba2dafa1f325691fb217511
Vladimir <le0679993@crc-lennox.qc.ca>
2023-11-22 14:26:14 -0500

    I fixed Munirs Script ... wtf ... chatgpt is hell of a drug

--- Assets/Scripts/betUI.cs
+++ Assets/Scripts/betUI.cs
@@ -9,88 +9,85 @@ public class betUI : MonoBehaviour
     [SerializeField] private Text currentBetText;
     [SerializeField] private Text currentUserMoneyText;
 
-    int[] betArray = {1, 5, 10, 25, 50, 100};
+    int[] betArray = { 1, 5, 10, 25, 50, 100 };
 
     int currentBet = 1;
     int currentBetAmount = 0;
-    int currentBetIndex = 0;
+
     int userMoney = 500;
-    
+
+    int counter;
     // Start is called before the first frame update
     void Start()
     {
+        counter = 0;
         currentBetText.text = string.Format("CURRENT BET: {0} $", currentBetAmount);
         betAmountText.text = string.Format("{0}", currentBet);
         currentUserMoneyText.text = string.Format("USER MONEY: {0} $", userMoney);
+        currentBet = betArray[counter];
 
-        FindCurrentBetAmount();
     }
 
-    private void FindCurrentBetAmount()
-    {
-        bool betAmountFound = false;
-        int counter = 0;
-        
-        while (!betAmountFound)
-        {
-            if (betArray[counter] == int.Parse(betAmountText.text))
-            {
-                betAmountFound = true;
-                currentBet = betArray[counter];
-                currentBetIndex = counter;
-                Debug.Log("COUNRTER"+counter.ToString());
-            }
-            if (counter < 5)
-            {
-                counter++;
-            }
-            
-                
-        }
-    }
     //ADD AN IF THAT CHECKS IF WE BETTING LESS THAN POOL MONEY
     public void increaseBet()
     {
-        FindCurrentBetAmount();
 
-        if(!(currentBet == 100))
+        if (counter < 5)
         {
-            //currentBetIndex += 1; 
-            currentBet = betArray[currentBetIndex + 1];
-            Debug.Log("INDEX" +currentBetIndex.ToString());   
+            counter++;
         }
+        currentBet = betArray[counter];
+        Debug.Log("counter" + counter.ToString());
+        Debug.Log("CurrentBET" + currentBet.ToString());
+
 
         betAmountText.text = string.Format("{0}", currentBet);
     }
 
     public void decreaseBet()
     {
-        FindCurrentBetAmount();
 
-        if (!(currentBet == 1))
+        if (counter > 0)
         {
-            //currentBetIndex -= 1;
-            currentBet = betArray[currentBetIndex - 1];
+            counter--;
         }
+        currentBet = betArray[counter];
+        Debug.Log("counter" + counter.ToString());
+        Debug.Log("CurrentBET" + currentBet.ToString());
 
-        betAmountText.text = string.Format("{0}", currentBetAmount);
+
+
+        betAmountText.text = string.Format("{0}", currentBet);
+    }
+    public void makeBet()
+    {
+        if (currentBet <= userMoney)
+        {
+            currentBetAmount += currentBet;
+            userMoney -= currentBet;
+
+            currentBetText.text = string.Format("CURRENT BET: {0} $", currentBetAmount);
+            currentUserMoneyText.text = string.Format("USER MONEY: {0} $", userMoney);
+        }
+        else
+        {
+            Debug.Log("You don't have enough money to make this bet.");
+        }
     }
 
     public void removeBet()
     {
-        userMoney += currentBetAmount;
-        currentBetAmount = 0;
-
-        currentBetText.text = string.Format("CURRENT BET: {0} $", currentBetAmount);
-    }
-
-    public void makeBet()
-    {
-        //GameManager.MakeBet = true;
-        currentBetAmount += currentBet;
-        userMoney -= currentBet;
+        if (currentBetAmount >= currentBet)
+        {
+            userMoney += currentBet;
+            currentBetAmount -= currentBet;
+        }
+        else
+        { Debug.Log("You cant remove more bets than you have in your betting stake.");
+        }
 
         currentBetText.text = string.Format("CURRENT BET: {0} $", currentBetAmount);
         currentUserMoneyText.text = string.Format("USER MONEY: {0} $", userMoney);
     }
+
 }
